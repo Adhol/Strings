020e2c48466702de04b7299037a9b3c8f54ff61f
AlekseyKholmogorov <adhol1989@gmail.com>
2019-04-29 21:54:45 +0400

    add task 66

--- src/Task66.java
+++ src/Task66.java
@@ -4,18 +4,40 @@
  * состоящие только из цифр, и объедините эти подстроки в одну новую строку.
  */
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
-
+/*For example
+  str1 qwe000005464qwe10000
+  str2 qwe0000000000033fdffd5000
+*/
 public class Task66 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter first string");
-        String s1 = in.nextLine().toLowerCase();
+        String[] s1 = in.nextLine().split("\\D");
         System.out.println("Enter second string");
-        String s2 = in.nextLine().toLowerCase();
+        String[] s2 = in.nextLine().split("\\D");
 
+        ArrayList<String> numbers1 = new ArrayList<>();
+        ArrayList<String> numbers2 = new ArrayList<>();
+
+        for (String word : s1) {
+            numbers1.add(word);
+        }
+
+        for (String word : s2) {
+            numbers2.add(word);
+        }
+
+
+        String max = Collections.max(numbers1, Comparator.comparing(s -> s.length())) +
+                     Collections.max(numbers2, Comparator.comparing(s -> s.length()));
+
+        System.out.println(max);
 
     }
 }
